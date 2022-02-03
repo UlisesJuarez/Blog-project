36a48ed94864312c2a544656d423b91fd5631116
UlisesJE <ujuareze1700@alumno.ipn.mx>
2022-02-02 20:27:05 -0600

    final version

--- app.js
+++ app.js
@@ -3,8 +3,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-const moongose=require("mongoose")
-const _=require("lodash");
+const moongose = require("mongoose")
+const _ = require("lodash");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -13,77 +13,69 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 moongose.connect("mongodb+srv://admin-ulises:Test-uli123@cluster0.0ayal.mongodb.net/BlogProject", { useNewUrlParser: true });
 
-const postSchema={
-  title:String,
-  content:String
+const postSchema = {
+  title: String,
+  content: String
 }
 
-const Post=moongose.model("Post",postSchema);
+const Post = moongose.model("Post", postSchema);
 
-app.get("/", function(req, res){
+app.get("/", function (req, res) {
 
-  Post.find({}, function(err, posts){
+  Post.find({}, function (err, posts) {
     res.render("home", {
       startingContent: homeStartingContent,
       posts: posts
-      });
+    });
   });
 
 });
 
-app.get("/contact",function(req,res){
-  res.render("contact",{contactI:contactContent});
+app.get("/contact", function (req, res) {
+  res.render("contact", { contactI: contactContent });
 });
 
-app.get("/about",function(req,res){
-  res.render("about",{aboutC:aboutContent});
+app.get("/about", function (req, res) {
+  res.render("about", { aboutC: aboutContent });
 })
 
-app.get("/compose",function(req,res){
-  res.render("compose",{aboutC:aboutContent});
+app.get("/compose", function (req, res) {
+  res.render("compose", { aboutC: aboutContent });
 });
 
-app.get("/posts/:postName",function(req,res){
-  const requestedTitle=_.lowerCase(req.params.postName);
-  
-  post.forEach(function(item){
-    const itemTitle=_.lowerCase(item.title);
+app.get("/posts/:postId", function (req, res) {
+  const requestedId = req.params.postId
 
-    if(itemTitle===requestedTitle){
-      res.render("post",{
-        title:item.title,
-        content:item.content
-      })
+  Post.findOne({ _id: requestedId}, function (err, post) {
+    res.render("post", {
+      title: post.title,
+      content: post.content
+    });
+
+  });
+
+})
+
+app.post("/compose", function (req, res) {
+  const post = new Post({
+    title: req.body.itemT,
+    content: req.body.description
+  });
+  post.save(function (err) {
+    if (!err) {
+      res.redirect("/");
     }
-
-  });
-
-})
-
-app.post("/compose",function(req,res){
-  const post=new Post({
-    title:req.body.itemT,
-    content:req.body.description
-  });
-  post.save()
-  res.redirect("/");
+  })
 });
-
-
-
-
-
-
-
-
-
-
-
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+let port=process.env.PORT;
+if(port==null || port==""){
+    port=3000;
+}
+app.listen(port, function () {
+    console.log("Server running successfully");
 });
